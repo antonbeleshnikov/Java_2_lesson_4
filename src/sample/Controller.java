3ceca38307065b05e2f571cb8ba5c28d8c4b93cc
Anton Beleshnikov <antonbeleshnikov@yandex.ru>
2021-08-01 17:42:09 +0300

    Заготовка для чата

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,22 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
 
 public class Controller {
-    public void sendHello(ActionEvent actionEvent) {
-        System.out.println("Hello");
-    }
+    @FXML
+    public TextField textField;
+    @FXML
+    public TextArea textArea;
 
+    @FXML
     public void sendMessage(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
     }
 }
