c05daa1a95578c5cfa13e3ccc6096ca3ffab161b
Anton Beleshnikov <antonbeleshnikov@yandex.ru>
2021-08-01 15:36:46 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void sendHello(ActionEvent actionEvent) {
+        System.out.println("Hello");
+    }
+
+    public void sendMessage(ActionEvent actionEvent) {
+    }
+}
